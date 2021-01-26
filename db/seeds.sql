e53b334dad9406c469423b8c7730ff0723e5d219
Paul Keldsen <Pkeld148@gmail.com>
2021-01-26 04:37:37 -0500

    copied package.json and installed packages, server.js with requirements, schema and seed database files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+insert into burgers (burger_name) values ("Single"), ("Double"), ("Triple");
+
+insert into burgers (burger_name, devoured) values ("Bacon Cheeseburger", true), ("Whopper", true);
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access
+// TODO: FINISH RELATIVE PATH HERE
+const routes = require()
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on http://localhost:" + PORT);
+}
