76c73de2a8344d51bb39f6bee19f3f9f4f8c079a
Paul Keldsen <Pkeld148@gmail.com>
2021-01-27 04:06:20 -0500

    omg i give up

--- config/connection.js
+++ config/connection.js
@@ -15,10 +15,10 @@ if (process.env.JAWS_URL) {
 
 
 connection.connect(function (err) {
-  if (err) {
-    console.error("Error connecting: " + err.stack);
-    return;
-  }
+  // if (err) {
+  //   console.error("Error connecting: " + err.stack);
+  //   return;
+  // }
   console.log("Connected as id " + connection.threadId);
 });
 
